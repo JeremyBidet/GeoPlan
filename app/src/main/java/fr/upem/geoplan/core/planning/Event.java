0d5cebe137e1dc50dd23bb562d56fa8fd77e27c1
Jeremy bidet <jeremy.bidet@gmail.com>
2016-02-11 18:20:37 +0100

    Update Planning module

--- app/src/main/java/fr/upem/geoplan/core/planning/Event.java
+++ app/src/main/java/fr/upem/geoplan/core/planning/Event.java
@@ -12,7 +12,7 @@ import java.util.Date;
  */
 public class Event {
 
-    private final long id = 0;
+    private final long id;
     private String name;
     private DateFormat start_date_time;
     private DateFormat end_date_time;
@@ -24,5 +24,27 @@ public class Event {
     private String description;
     private Weight weight;
     private Type type;
+
+    private Event(long id, String name, DateFormat start_date_time, DateFormat end_date_time, String localization, float pos_lat, float pos_lon, ArrayList<Long> guests, ArrayList<Long> owners, String description, Weight weight, Type type) {
+        this.id = id;
+        this.name = name;
+        this.start_date_time = start_date_time;
+        this.end_date_time = end_date_time;
+        this.localization = localization;
+        this.pos_lat = pos_lat;
+        this.pos_lon = pos_lon;
+        this.guests = guests;
+        this.owners = owners;
+        this.description = description;
+        this.weight = weight;
+        this.type = type;
+    }
+
+
+    public static Event getEvent(long id) {
+        //TODO: get from database the event by its event id
+
+        return new Event(id, null, null, null, null, 0, 0, null, null, null, null, null);
+    }
 
 }
