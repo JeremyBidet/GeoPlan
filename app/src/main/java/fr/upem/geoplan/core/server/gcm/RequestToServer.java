be7c4c6157177e5e40f69b5013d9193e02e81ab3
Jérémie Chattou <jj.chattou@gmail.com>
2016-03-07 19:20:09 +0100

    Signature méthode

--- app/src/main/java/fr/upem/geoplan/core/server/gcm/RequestToServer.java
+++ app/src/main/java/fr/upem/geoplan/core/server/gcm/RequestToServer.java
@@ -1,6 +1,46 @@
 package fr.upem.geoplan.core.server.gcm;
 
 
-public class RequestToServer {
+import com.google.android.gms.maps.model.LatLng;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class RequestToServer {
+    public static void createUser(long userId, Map<String, Object> userJSON) {
+    }
+
+    public static void createEvent(long eventId, Map<String, Object> eventJSON) {
+    }
+
+    public static ArrayList<Long> getAllEvents(long userId) {
+        return null;
+    }
+
+    public static LatLng getPosition(long userID) {
+        return null;
+    }
+
+    public static void updatePositionUser(LatLng position, long userId) {
+    }
+
+    public static void addUserToEvent(long userId, long eventId) {
+    }
+
+    public static void addUsersToEvent(List<Long> guestsId, long eventId) {
+    }
+
+    public static void removeUserToEvent(long userId, long eventId) {
+    }
+
+    public static void removeUsersToEvent(List<Long> guestsId, long eventId) {
+
+    }
+
+    public static void updateEvent(long eventId, Map<String, Object> eventJSON) {
+    }
+
+    public static void updateEvent(long eventId, ArrayList<Long> ownersId, String title, String description, ArrayList<Long> guestsId, long startDateTime, long endDateTime, String localization, LatLng position) {
+    }
 }
