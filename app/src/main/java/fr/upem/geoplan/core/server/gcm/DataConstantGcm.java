d848a1942e236809cab29c9381bd78ed7807decf
Jérémie Chattou <jj.chattou@gmail.com>
2016-03-10 15:01:08 +0100

    Client to Server

---
+++ app/src/main/java/fr/upem/geoplan/core/server/gcm/DataConstantGcm.java
@@ -0,0 +1,26 @@
+package fr.upem.geoplan.core.server.gcm;
+
+/**
+ * Created by Jeremie on 08/03/2016.
+ */
+public class DataConstantGcm {
+    //General constant
+    public static String ACTION = "action";
+    public static String POSITION = "position";
+    public static String POSITION_LATITUDE = "latitude";
+    public static String POSITION_LONGITUDE = "longitude";
+
+    // User constant
+    public static String FIRST_NAME = "firstName";
+    public static String LAST_NAME = "lastName";
+    public static String PHONE = "phone";
+
+    // Event constant
+    public static String EVENT_ID = "eventId";
+    public static String DESCRIPTION = "description";
+    public static String LOCALIZATION = "localization";
+    public static String START_DATE_TIME =  "startDateTime";
+    public static String END_DATE_TIME =  "startDateTime";
+    public static String GUESTS_ID =  "guestsId";
+
+}
