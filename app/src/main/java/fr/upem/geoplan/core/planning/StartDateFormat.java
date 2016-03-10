eaf82055be2203493a80003cb91e4298a9673d36
Git-Huy <chinook_@hotmail.fr>
2016-03-10 02:32:19 +0100

    startdateformat
    remplacement de getMilliInDay qui renvoyait toujours un chiffre négatif
    et ajout de 0 devant la minute ou l'heure lorsque celle ci est comprise
    entre 0 et 9

--- app/src/main/java/fr/upem/geoplan/core/planning/StartDateFormat.java
+++ app/src/main/java/fr/upem/geoplan/core/planning/StartDateFormat.java
@@ -1,39 +1,43 @@
 package fr.upem.geoplan.core.planning;
 
+import android.util.Log;
+
 import java.text.DateFormat;
 import java.text.FieldPosition;
 import java.text.ParsePosition;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by pierre on 06/03/2016.
  */
 public class StartDateFormat extends DateFormat {
 
-    private long getMilliInDay(int day) {
-        final Calendar closeDays = Calendar.getInstance();
-        closeDays.set(0, 0, day);
-
-        return closeDays.getTimeInMillis();
-    }
-
     private boolean isCloseDay(Calendar calendar, Calendar now) {
-        return Math.abs(calendar.getTimeInMillis() - now.getTimeInMillis()) < getMilliInDay(1);
+        return Math.abs(calendar.getTimeInMillis() - now.getTimeInMillis()) <  TimeUnit.DAYS.toMillis(1);
     }
 
     private StringBuffer formatCloseDay(Calendar calendar, Calendar now, StringBuffer buffer) {
         final Long delta = calendar.getTimeInMillis() - now.getTimeInMillis();
-        final Long deltaDays = delta / getMilliInDay(1);
+        final Long deltaDays = delta / TimeUnit.DAYS.toMillis(1);
 
         switch (deltaDays.intValue()) {
             default:
                 return buffer;
             case 0:
+                String date_min=":%d";
+                String date_hour="%d";
+                if(calendar.get(Calendar.MINUTE) >= 0 && calendar.get(Calendar.MINUTE) <10) {
+                    date_min = ":0%d";
+                }
+                if(calendar.get(Calendar.HOUR_OF_DAY) >= 0 && calendar.get(Calendar.HOUR_OF_DAY) <10) {
+                    date_hour = "0%d";
+                }
                 if (delta > 0) {
-                    return buffer.append(String.format("tomorrow at %d:%d", calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE)));
+                        return  buffer.append(String.format("tomorrow at " + date_hour + date_min, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE)));
                 } else {
-                    return buffer.append(String.format("yesterday at %d:%d", calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE)));
+                        return buffer.append(String.format("yesterday at " + date_hour + date_min, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE)));
                 }
         }
 
@@ -69,7 +73,7 @@ public class StartDateFormat extends DateFormat {
                         .append(String.format("in %d days", deltaDays));
             } else {
                 buffer
-                        .append(String.format("there is %d days", deltaDays));
+                        .append(String.format("%d days ago", deltaDays));
             }
         }
 
